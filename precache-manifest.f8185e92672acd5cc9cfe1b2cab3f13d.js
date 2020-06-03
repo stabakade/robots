2bebec6190ed2d43fc1415d3cd62a19f3dd8e25a
Sushrut Tabakade <sushrut.tab1998@gmail.com>
2020-06-03 16:18:10 +0530

    Updates

---
+++ precache-manifest.f8185e92672acd5cc9cfe1b2cab3f13d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d063906aed208915a606b8ab9977e0",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "962906164b5631489f63",
+    "url": "/robots/static/css/main.edbb37db.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robots/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "962906164b5631489f63",
+    "url": "/robots/static/js/main.c74862b1.chunk.js"
+  },
+  {
+    "revision": "67da454f3f0e82d7753f",
+    "url": "/robots/static/js/runtime-main.4460835f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
